74e4dd09dfa8052588b2a971b6b9fdf0aa5dc436
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2024-02-08 19:38:59 +0000

    Chapter2

---
+++ Challenges/Chapter2.php
@@ -0,0 +1,14 @@
+<?php
+	// Challenge #1
+	// Broke up the command into multiple lines
+	$text = "SQUIRREL!";
+	$text = strtolower($text);
+	$text = strrev($text);
+	$text = str_replace('ri', 'aa', $text);
+	echo $text;
+?>
+
+<?php
+	// Challenge #2
+	echo strlen("fluffy kitten");
+?>
