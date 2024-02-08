49bcf837b0889f6172338b5f4ef72069e407113e
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2024-02-08 19:41:52 +0000

    Chapter3

---
+++ Challenges/Chapter3.php
@@ -0,0 +1,11 @@
+<?php
+	// Challenge #1
+	$brands = ['SuperDog', 'Doggie-Os', 'Beef Bites', 'Puppy Gruel', 'Fancy Feast'];
+	echo $brands[3];
+?>
+
+<?php
+	$array = array_reverse(array('🍎', '🍌', '🍕', '🎂'), true);
+	$array = array_reverse($array);
+	echo $array[2];
+?>
